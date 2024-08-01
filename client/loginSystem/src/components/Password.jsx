98cbcc1b6b57bd991c24e301c537f2c4b9ee5227
sumit1232 <sumitjadav2525@gmail.com>
2024-08-01 17:32:06 +0530

    password Field Required- 50min

--- client/loginSystem/src/components/Password.jsx
+++ client/loginSystem/src/components/Password.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import avatar from '../assets/profile.png'
+import { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import styles from '../styles/Username.module.css';
+import {passwordValidate} from '../helper/validate.js'
 
 function Password() {
+
+  const formik = useFormik({
+    initialValues : {
+      password : 'admin@123'
+    },
+    validate: passwordValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit : async values => {
+      console.log(values);
+    }
+  })
+
+
   return (
-    <div>Password</div>
+    <div className="container mx-auto">
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
+<div className='flex justify-center item-center h-screen'>
+<div className={styles.glass}>
+
+<div className='title flex flex-col items-center'>
+<h4 className='text-5xl font-bold'>Hello Again</h4>
+<span className='py-4 text-xl w-2/3 text-center text-gray-500'> 
+Explore More by Connecting with Us.
+</span>
+</div>
+
+<form className='py-1' onSubmit={formik.handleSubmit}>
+  <div className='profile flex justify-center py-4'>
+<img  src={avatar} className={styles.profile_img} alt='avatar'/>
+  </div>
+
+  <div className='textbox flex flex-col items-center gap-6'>
+    <input {...formik.getFieldProps('password')} className={styles.textbox} type='text' placeholder='password'/>
+    <button className={styles.btn} type='submit'>Let's Go</button>
+  </div>
+
+<div className="text-center py-4">
+<spam>Forgot Password ?<Link className='text-red-500' to='/recovery'>Recover Now</Link></spam>
+</div>
+
+</form>
+
+
+
+</div>
+</div>
+    </div>
   )
 }
 
--- client/loginSystem/src/helper/validate.js
+++ client/loginSystem/src/helper/validate.js
@@ -6,12 +6,40 @@ export async function usernameValidate(values){
     return errors;
 }
 
+
+/** validate password */
+function passwordVerify(errors = {}, values){
+    /* eslint-disable no-useless-escape */
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if(!values.password){
+        errors.password = toast.error("Password Required...!");
+    } else if(values.password.includes(" ")){
+        errors.password = toast.error("Wrong Password...!");
+    }else if(values.password.length < 4){
+        errors.password = toast.error("Password must be more than 4 characters long");
+    }else if(!specialChars.test(values.password)){
+        errors.password = toast.error("Password must have special character");
+    }
+
+    return errors;
+}
+
+/** validate password */
+export async function passwordValidate(values){
+    const errors = passwordVerify({}, values);
+
+    return errors;
+}
+
+
+
 // Validate Username 
 function usernameVerify(error = {}, values){
 if(!values.username){
 error.username = toast.error("Username Required");
 }
-else if(values.username.includes(" ")){
+else if(values.username.includes("")){
     error.username = toast.error('Invalid Username');
 }
 return error;
